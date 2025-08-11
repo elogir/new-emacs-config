f4c9972d162d1d5a20b9ef034440abe786fa7af1
raphael.nahoum <raphael.nahoum@epita.fr>
2025-08-11 21:17:47 +0200

    First Commit

---
+++ early-init.el
@@ -0,0 +1,43 @@
+(setq package-enable-at-startup nil)
+(setq inhibit-default-init nil)
+(setq native-comp-async-report-warnings-errors nil)
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 1)
+
+(defun +gc-after-focus-change ()
+  "Run GC when frame loses focus."
+  (run-with-idle-timer
+   5 nil
+   (lambda () (unless (frame-focus-state) (garbage-collect)))))
+
+(defun +reset-init-values ()
+  (run-with-idle-timer
+   1 nil
+   (lambda ()
+     (setq file-name-handler-alist default-file-name-handler-alist
+           gc-cons-percentage 0.1
+           gc-cons-threshold 100000000)
+     (message "gc-cons-threshold & file-name-handler-alist restored")
+     (when (boundp 'after-focus-change-function)
+       (add-function :after after-focus-change-function #'+gc-after-focus-change)))))
+
+(with-eval-after-load 'elpaca
+  (add-hook 'elpaca-after-init-hook '+reset-init-values))
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(setq server-client-instructions nil)
+
+(setq frame-inhibit-implied-resize t)
+
+(push '(font . "Source Code Pro") default-frame-alist)
+(set-face-font 'default "Source Code Pro")
+(set-face-font 'variable-pitch "DejaVu Sans")
+(copy-face 'default 'fixed-pitch)
+
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+(setq ring-bell-function #'ignore
+      inhibit-startup-screen t)
